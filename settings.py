5a7d34838b11e5142663b972e4cd69d14925b4e9
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-07 17:59:26 -0600

    Moving Ship Left & Right

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         """Initializaing game settings"""
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (230, 230, 230)  ## tuple
--- ship.py
+++ ship.py
@@ -20,6 +20,17 @@ class Ship:
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        ##movement
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement Flag"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw the ship to the screen at the position specified by self.rect."""
         self.screen.blit(self.image, self.rect)
