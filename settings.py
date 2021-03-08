832629245e39fcad998af069bf30381ccc6abbe0
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-07 18:32:39 -0600

    Adjusting ship speed

--- settings.py
+++ settings.py
@@ -6,3 +6,4 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)  ## tuple
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -17,8 +17,13 @@ class Ship:
             self.image.get_rect()
         )  ## accessing ships surface rectangle attribute
 
+        self.settings = ai_game.settings
+
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a decimal value form the ship's horizontal position.
+        self.x = float(self.rect.x)
 
         ##movement
         self.moving_right = False
@@ -26,10 +31,14 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement Flag"""
+        # update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # update rect object from the self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship to the screen at the position specified by self.rect."""
