8555b5566b591dc0f736ee362f631e9268635019
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-07 19:30:16 -0600

    Creating,Storing&Firing bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -26,6 +27,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     ## the game is controlled  by run game method
     def run_game(self):
@@ -33,6 +35,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()  # ship movement
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):  ##simplyfing the run_game method
@@ -56,6 +59,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:  # pressing q to Quit
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases """
@@ -64,11 +69,19 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -3,7 +3,13 @@ class Settings:
 
     def __init__(self):
         """Initializaing game settings"""
-        # self.screen_width = 1200  # not needed after keeping in full screen
-        # self.screen_height = 600 # not needed after keeping in full screen
+        self.screen_width = 1200  # not needed after keeping in full screen
+        self.screen_height = 600  # not needed after keeping in full screen
         self.bg_color = (230, 230, 230)  ## tuple
         self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
