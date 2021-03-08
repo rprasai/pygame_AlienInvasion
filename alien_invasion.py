937ae010f5d990a6b51f823da0a1b49849d63262
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-07 18:52:28 -0600

    Full Screen mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,13 +15,14 @@ class AlienInvasion:
         pygame.init()  ## initializes the background settings
 
         self.settings = Settings()
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         ## pygame.display.set_mode create display window for the game where games graphical element will be drawn
         ##(12000 ##pixel wide, 800 ## pixel wide) ##(1200, 800)is a tuple
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
 
     def __init__(self):
         """Initializaing game settings"""
-        self.screen_width = 1200
-        self.screen_height = 600
+        # self.screen_width = 1200  # not needed after keeping in full screen
+        # self.screen_height = 600 # not needed after keeping in full screen
         self.bg_color = (230, 230, 230)  ## tuple
         self.ship_speed = 1.5
