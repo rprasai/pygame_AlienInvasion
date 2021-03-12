94475a05d57f6ef2f00c43cce24e61f7076b48a1
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-11 20:15:36 -0600

    Creating Row of Aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,7 +93,20 @@ class AlienInvasion:
         """Create teh fleet of aliens"""
         # Make an ALien.
         alien = Alien(self)
-        self.aliens.add(alien)
+        # self.aliens.add(alien)
+        # create an alien and find the number of aliens in a row.
+        # spacing between each alien is equal to one alien weidth
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create teh first row of aliens.
+        for alien_number in range(number_aliens_x):
+            # create an alien and place it in the row.
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
