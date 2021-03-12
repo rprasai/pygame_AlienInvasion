157d1c73e048a0f9d93674be9db0c36de52dd121
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-12 05:11:02 -0600

    Aliens: Checking Edges and Changing Directions

--- alien_invasion.py
+++ alien_invasion.py
@@ -123,8 +123,24 @@ class AlienInvasion:
         self.aliens.add(alien)
 
     def _update_aliens(self):
+        """Check if the fleet is at an edge,
+        then update teh postions of all aliens in the fleet"""
+        self._check_fleet_edges()
         """Update the positions of all aliens in the fleet"""
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriatelty if any aliesn have reached an edge. """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
--- settings.py
+++ settings.py
@@ -17,3 +17,6 @@ class Settings:
 
         # Alien Settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of q represents right; -1 represents left
+        self.fleet_direction = 1
