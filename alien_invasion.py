0a5c6e0333f0046364f55b49f83b14f99a7af406
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-14 18:06:15 -0500

    Starting the Game with Click on Play

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,6 +66,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+
     def _check_keydown_events(self, event):
         """Respond to Keypresses."""
         if event.key == pygame.K_RIGHT:
