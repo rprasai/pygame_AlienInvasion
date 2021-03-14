8713bfa7bdad3aba70219a81427521189907f5d3
Ravi Prasai <raviprasai1994@gmail.com>
2021-03-14 16:42:39 -0500

    Refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,11 +89,21 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
         # print(len(self.bullets))
+
+    def _check_bullet_alien_collisions(self):
+        """REspond to bullet-aliens collisons"""
+        # Remove any bullets and alients that have collided
 
         # Check for any bullets the ahve hit aliens
         # if so, get rid of the bullets and teh alien
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            # Distroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
 
     def _create_fleet(self):
         """Create teh fleet of aliens"""
